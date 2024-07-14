36304f7f813801f19d29cdbe089bb67e9ca28bf4
JayG1212 <jaybgunderson@gmail.com>
2024-07-14 16:46:54 -0500

    Created project in VS

---
+++ Labeling Inventory (Polymorphism, Page 209)/Labeling Inventory (Polymorphism, Page 209)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Written by Jay Gunderson
+// 07/14/2024
+
+namespace Labeling_Inventory__Polymorphism__Page_209_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
